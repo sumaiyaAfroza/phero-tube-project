81af09fc8fa8b0bf2e8bb49f06f3346c2a2e5189
alfa96git <alfa.sunny.96@gmail.com>
2025-03-18 02:03:33 +0600

    added details

--- script/index.js
+++ script/index.js
@@ -1,12 +1,20 @@
-console.log(1)
+
+const showLoader =()=>{
+  document.getElementById('loader').classList.remove('hidden')
+  document.getElementById('video-container').classList.add('hidden')
+}
+const hideLoader =()=>{
+  document.getElementById('loader').classList.add('hidden')
+  document.getElementById('video-container').classList.remove('hidden')
+}
+
+
 function removeActiveClass (){
   const activeButton = document.getElementsByClassName('active')
   for(let btn of activeButton){
     btn.classList.remove('active')
   }
 }
-
-
 function loadCategories() {
 
     //1. fetch the data
@@ -18,19 +26,19 @@ function loadCategories() {
         // 3. send data to display
         .then(data => displayCategories(data.categories))
 }
-
-function loadVideos() {
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+function loadVideos(searchText = "") {
+  showLoader()
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
         .then(response => response.json())
         .then(data => {
-          removeActiveClass()
-          document.getElementById('btn-all').classList.add('active')
-          displayVideos(data.videos)
+          removeActiveClass();
+          document.getElementById('btn-all').classList.add('active');
+          displayVideos(data.videos);
         })
 }
-
+// 1001
 const loadCategoryVideos =(id) => {
-    
+    showLoader()
     const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
     console.log(url);
 
@@ -38,13 +46,41 @@ const loadCategoryVideos =(id) => {
     .then(res => res.json())
     .then(data=> {
       removeActiveClass();
-
       const clickedButton = document.getElementById(`btn-${id}`); 
       clickedButton.classList.add('active');
-     
       displayVideos(data.category);
-    })
+    });
+   
 }
+const loadVideoDetails =(videoId)=>{
+  console.log(videoId)
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
+  console.log(url);
+  fetch(url)
+  .then(res => res.json())
+  .then( data =>{
+    displayVideosDetails(data.video)
+  })
+}
+const displayVideosDetails = (video) => {
+  console.log(video)
+  document.getElementById('video_details').showModal()
+  const detailsContainer = document.getElementById('details-container')
+  detailsContainer.innerHTML=`
+  <div class="card bg-base-100 image-full w-96 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>${video.description}</p>
+  </div>
+</div>
+  `;
+}
+
 // {category_id: '1001', category: 'Music'}
 // for of diye 
 function displayCategories(categories) {
@@ -91,13 +127,14 @@ const displayVideos = (videos) => {
     const videoContainer = document.getElementById('video-container');
     videoContainer.innerHTML= "";
     if(videos.length == 0){
-      
+
       videoContainer.innerHTML =`
       <div class=" col-span-full flex flex-col justify-center items-center text-center m-12">
             <img src="./assets/Icon.png" alt="">
             <h2 class="text-2xl font-bold mt-4">Oops!! Sorry, There is no content here</h2>
         </div>
       `;
+      hideLoader()
       return;
     }
     videos.forEach((video) => {
@@ -111,8 +148,7 @@ const displayVideos = (videos) => {
                 src="${video.thumbnail}"
                 alt="Shoes" />
                 <span class="absolute bottom-2 right-2 bg-black text-white px-2 text-sm rounded-sm">3hrs 56 min ago</span>
-            </figure>
-            
+            </figure>  
             <div class=" flex gap-5 px-0 py-5">
               <div class="profile">
                 <div class="avatar">
@@ -122,15 +158,28 @@ const displayVideos = (videos) => {
                   </div>
               </div>
               <div class="intro space-y-1">
-                <h2 class="text-md font-semibold">Smells Like Teen Spirit</h2>
-                <p class="text-gray-500 text-sm flex gap-1">${video.authors[0].profile_name} <img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt=""></p>
+                <h2 class="text-md font-semibold">${video.title}</h2>
+
+                <p class="text-gray-500 text-sm flex gap-1">${video.authors[0].profile_name}
+                ${video.authors[0].verified === true ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt=""></p>`:``}
+                 
+
                 <p class="text-gray-500 text-sm">${video.others.views}</p>
+            </div>         
             </div>
-            </div>
+            <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block">show Details</button>
           </div>
         `;
         // append
         videoContainer.append(videoCard)
-    })
+    });
+    hideLoader()
 }
+
+document.getElementById('search-input').addEventListener('keyup',(e) =>{
+  const input = e.target.value;
+  loadVideos(input)
+})
+
+
 loadCategories()
